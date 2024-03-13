d7a230c852beca0bfb1b1804a09822004dcc9bb6
developerjame <119099522+developerjame@users.noreply.github.com>
2024-03-13 13:09:06 +0300

    question

---
+++ Pow(x, n)/solution.php
@@ -0,0 +1,29 @@
+<?php
+class Solution {
+
+    /**
+     * @param Float $x
+     * @param Integer $n
+     * @return Float
+     */
+    function myPow($x, $n) {
+        if($n == 0) return 1;
+        if($n < 0){
+            $x = 1/$x;
+            $n = -$n;
+        }
+        return $this->fastPow($x, $n);
+    }
+    function fastPow($x, $n){
+        if($n == 0) return 1;
+        $half = $this->fastPow($x, $n/2);
+        if($n%2 == 0){
+            return $half*$half;
+        }else{
+            return $half*$half*$x;
+        }
+    }
+}
+$solution = new Solution();
+echo $solution->myPow(1.0000000000001, -2147483648);
+?>
